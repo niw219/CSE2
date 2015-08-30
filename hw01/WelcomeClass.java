ec41814735462d9ff47d46119a8136571ea8b657
niw219 <niw219@lehigh.edu>
2015-08-30 14:44:17 +0000

    Updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,19 @@
+//define a class
+public class WelcomeClass{
+    
+//add main method
+public static void main(String[] args) {
+    
+System.out.println("  ----------- ");
+System.out.println("  | WELCOME |");
+System.out.println("  -----------");
+System.out.println("  ^  ^  ^  ^  ^  ^");
+System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+System.out.println("|-N--I--W--2--1--9-|");
+System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+System.out.println("  v  v  v  v  v  v");
+System.out.println("I am a huge sushi enthusiast.");
+System.out.println("I love spicy tuna and avocado.");
+
+}
+};
